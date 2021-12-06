8faf52080e55f6e0940b0adcfcc986e5ac0db85d
GrukDan <den.gruk@mail.ru>
2021-12-06 21:41:40 +0300

    bootstrap project

---
+++ university-service/src/test/java/edu/gruk/university/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gruk.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
